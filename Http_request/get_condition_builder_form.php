dfc93b014e0145c8e9d88331cbbfe00ad0b07385
Sabaho <powerganoflove@hotmail.com>
2020-03-18 16:26:02 +0700

    clear Bug fix

--- Http_request/get_condition_builder_form.php
+++ Http_request/get_condition_builder_form.php
@@ -1,8 +1,90 @@
 <?php
+function parse_condition_to_WHERE_IN($array_condition,$fields_json){
+   // AND h1 IN ()
 
-function generate_main_sql_condition($conditions,$fields_json){// return main sql condition
+   // $array_key = array_keys($fields_json, "รายการ");
+   // $field_header = $array_key[0]+1;
+ 
+   // [field_name] => ID
+   // [value_type] => con_value
+   // [valuelist] => ฟหก
+
+   $array_same_fields = array();
+   $array_same_fields_count = array();
+
+   $array_count_same_fields = array();
+
+   $text = "";
+   
+   foreach($array_condition as $condition){
+
+      // $array_key = array_keys($fields_json,$condition['field_name']);
+      // $field_header = $array_key[0]+1;
+
+      if($condition['field_name'] == "รายการ"){
+
+      }
+      else{
+         $field_name = $condition['field_name'];
+      }
+     
+      array_push($array_same_fields,$field_name);
+   }
+
+   $array_count_same_fields = array_count_values($array_same_fields);
+   $array_count_same_fields_keys = array_keys($array_count_same_fields);
+
+   $count_same_condition = 1;
+
+   $obj_same_fields = new stdClass();
+
+   foreach($array_count_same_fields_keys as $same_field){
+      $obj_same_fields->{$same_field} = array();
+      $i = 1;
+      $count = $array_count_same_fields[$same_field];
+
+      for($i ; $i <= $count ; $i++){   
+         if($array_condition[$count_same_condition-1]['field_name'] == $same_field){
+            $obj_array = array(
+               'value_type'=>$array_condition[$count_same_condition-1]['value_type'],
+               'value'=>$array_condition[$count_same_condition-1]['valuelist']
+            );
+            array_push($obj_same_fields->{$same_field},$obj_array);
+         } 
+         $count_same_condition++;
+      }
+   }
+   
+   foreach($obj_same_fields as $key => $value) {
+      
+   }
+   return /*print_r($obj_same_fields)*/$text;
+
+}
+function count_same_fields_for_WHERE_IN($fields){
+
+   $count_same_fields = array_count_values($fields);
+   $fields_arary_keys = array_keys($count_same_fields);
+   $same_fields = array();
+
+   foreach($fields_arary_keys as $field){
+      if($count_same_fields[$field] > 1){
+         // array_push($same_fields,array(
+         //    'field_name'=>$field,
+         //    'fields_count'=>$count_same_fields[$field]
+         // ));
+         array_push($same_fields,$field);
+      }
+   }
+
+   return $same_fields;
+}
+function generate_main_sql_condition($conditions,$fields_json,$number_field){// return main sql condition
+
    $sql = '';
-   if(empty($conditions['oplist'])){// ตรวจสอบเงื่อนไขแรก จะไม่มี AND และ OR
+
+
+   if(empty($conditions['oplist']) || $number_field == 1){// ตรวจสอบเงื่อนไขแรก จะไม่มี AND และ OR
       $sql.= ' ';
    }
    else{
@@ -14,20 +96,20 @@ function generate_main_sql_condition($conditions,$fields_json){// return main sq
       $field_header = $array_key[0]+1;
       if($conditions['value_type'] == 'con_value'){
          if(is_numeric($conditions['valuelist'])){
-            $sql.= ' ( h'.$field_header.'_1';
+            $sql.= ' (h'.$field_header.'_1';
          }
          else{
-            $sql.= ' ( h'.$field_header.'_2';
+            $sql.= ' (h'.$field_header.'_2';
          }
       }
       else{
-         $sql.= ' ( h'.$field_header.'_1';
+         $sql.= ' (h'.$field_header.'_1';
       }         
    }
    else{
       $array_key = array_keys($fields_json, $conditions['fieldlist']);
       $field_header = $array_key[0]+1;
-      $sql.= ' ( h'.$field_header;
+      $sql.= ' (h'.$field_header;
    }
    $sql.= ' '.$conditions['condition_opv'];
    
@@ -57,27 +139,34 @@ function generate_main_sql_condition($conditions,$fields_json){// return main sq
       $field_header = $array_key[0]+1;
       if($conditions['value_type'] == 'con_value'){
          if(is_numeric($conditions['valuelist'])){
-            $sql.= ' AND h'.$field_header.'_1 != "" )';
+            $sql.= ' AND h'.$field_header.'_1 != "")';
          }
          else{
-            $sql.= ' )';
+            $sql.= ')';
          }
       }
       else{
-         $sql.= ' AND h'.$field_header.'_1 != "" )';
+         $sql.= ' AND h'.$field_header.'_1 != "")';
       }
    }
    else{
-      $sql.= ' )';
+      $sql.= ')';
    }
    return $sql;
 }
 
-function generate_sub_sql_condition($conditions,$fields_json){// return sub sql condition
-   $sql = ' '.$conditions['conjection_condition'];// ตัวเชื่อมระหว่างเงื่อนไขหลักกับเงื่อนไขรอง
-   $sql .= ' (';
+function generate_sub_sql_condition($conditions,$fields_json,$number_condition,$loop_non_same_fields_count){// return sub sql condition
+   $sql = '';
+   if(empty($conditions['conjection_condition']) || $loop_non_same_fields_count == 1){
+      $sql.= ' (';
+   }
+   else{
+      $sql .= ' '.$conditions['conjection_condition'];// ตัวเชื่อมระหว่างเงื่อนไขหลักกับเงื่อนไขรอง
+      $sql .= ' (';
+   }
+  
    foreach($conditions['sub_con'] as $sub_con){// วนลูปตามจำนวนของเงื่อนไขย่อย
-      if(empty($sub_con['sub_op_list'])){
+      if(empty($sub_con['sub_op_list']) || $number_condition == 1){
          $sql.= '';
       }
       else{
@@ -149,7 +238,83 @@ function generate_sub_sql_condition($conditions,$fields_json){// return sub sql 
    $sql.= ' )';
    return $sql;
 }
+function generate_sub_sql_condition2($sub_con,$fields_json,$number_condition){// return sub sql condition
+   $sql = '';
 
+  
+   if(empty($sub_con['sub_op_list']) || $number_condition == 1){
+      $sql.= '(';
+   }
+   else{
+      $sql.= $sub_con['sub_op_list'].' (';
+   }  
+ 
+   if($sub_con['sub_field_list'] == 'รายการ'){
+      $array_key = array_keys($fields_json, "รายการ");
+      $field_header = $array_key[0]+1;
+      if($sub_con['sub_condition_value_type'] == 'con_value'){
+         if(is_numeric($sub_con['sub_value_list'])){
+            $sql.= 'h'.$field_header.'_1';
+         }
+         else{
+            $sql.= 'h'.$field_header.'_2';
+         }
+      }
+      else{
+         $sql.= 'h'.$field_header.'_1';
+      }         
+   }
+   else{
+      $array_key = array_keys($fields_json, $sub_con['sub_field_list']);
+      $field_header = $array_key[0]+1;
+      $sql.= 'h'.$field_header;
+   }
+   $sql.= ' '.$sub_con['sub_condition_opv'];
+   
+   if($sub_con['sub_condition_value_type'] == 'con_value'){// ตรวจสอบ ชนิดของค่าเงื่อนไข ถ้าเป็น ตัวเลขหรือตัวอักษร
+      if(is_numeric($sub_con['sub_value_list'])){// ถ้าค่าเป็นตัวเลข
+         $sql.= ' '.$sub_con['sub_value_list'];
+      }
+      else{
+         $sql.= ' '.'"'.$sub_con['sub_value_list'].'"';
+      }
+   }
+   else{
+      if($sub_con['sub_value_list'] == 'รายการ'){// ถ้าค่าของเงื่อนไขเป็นฟีลด์ และมีค่าเป็น h2
+         $array_key = array_keys($fields_json, "รายการ");   
+         $field_header = $array_key[0]+1;
+         $sql.= ' h'.$field_header.'_1';
+      }
+      else{
+         $array_key = array_keys($fields_json, $sub_con['sub_value_list']);
+         $field_header = $array_key[0]+1;
+         $sql.= ' h'.$field_header;
+      }
+   }
+
+   if($sub_con['sub_field_list'] == 'รายการ'){
+      $array_key = array_keys($fields_json, "รายการ"); 
+      $field_header = $array_key[0]+1;
+      if($sub_con['sub_condition_value_type'] == 'con_value'){
+         if(is_numeric($sub_con['sub_value_list'])){
+            $sql.= ' AND h'.$field_header.'_1 != "") ';
+         }
+         else{
+            $sql.= ') ';
+         }
+      }
+      else{
+         $sql.= ' AND h'.$field_header.'_1 != "") ';
+      }
+   }
+   else{
+      $sql.= ') ';
+   }
+   
+
+   return $sql;
+
+}
 function generate_fields_WBS($fields_json){
    $sql = '';
    $array_key = array_keys($fields_json,"WBS");
@@ -273,7 +438,7 @@ function generate_where_condition_WBS_fields($fields_json){
    $fields_json = json_decode($_POST['fields_count']);
    $sql = '';
    $sql_where = '';
-
+   
    if(in_array("WBS",$fields_json)){ // ตรวจถ้ามีฟีลด์ชื่อว่า WBS 
       $sql .= 'SELECT '.generate_fields_WBS($fields_json) . ' FROM '.'`'.$table.'`';
       if(!empty($_POST['condition_type_row'])){ 
@@ -318,23 +483,6 @@ function generate_where_condition_WBS_fields($fields_json){
          else{
             $response['query_data'] = false;
          }
-        // $response['outer_loop_sql'] = $sql_get_WBS;
-
-         /*
-          เก็บ WBS ไว้ในตัวแปร global
-          foreach $WBS และ query WBS แต่ละอันว่ามีงานอะไรบ้างที่ตรง โดยอิงเงื่อนไขตามที่ User กำหนดมาจาก form
-          และ เก็บข้อมูลอยูาในรูปแบบ JSON Oject
-         "WBS" => "AYY"{
-                        0:{dsd:zxc,dasds:12323},
-                        1:{dsd:zxc,dasds:12323},
-                        1:{dsd:zxc,dasds:12323}
-                     }, 
-                     "KNN"{
-                        ,,
-                     }
-         
-         ถ้างานไหนไม่มี WBS ให้ WBS = false
-          */
       }
       else{
          $sql_get_WBS = $sql.generate_last_sql($fields_json);
@@ -380,58 +528,146 @@ function generate_where_condition_WBS_fields($fields_json){
       $sql.= $sql_where;     
       $response['task_type'] = "WBS_task";
    }
-   else{
-      $sql .= 'SELECT '.generate_fields($fields_json) . ' FROM '.'`'.$table.'`';
+   else{ // have conditions
+      $sql .= 'SELECT '.generate_fields($fields_json) . ' FROM '.'`'.$table.'`'; //generate attributes
       if(!empty($_POST['condition_type_row'])){ 
          $sub_row_data_count = json_decode($_POST['sub_row_data_count']);// รับข้อมูล JSON ข้อมูลจำนวนเงื่อนไขของเงื่อนไขย่อย
-   
+
+         if(!empty($_POST['main_fieldlist'])){
+            $same_fields = count_same_fields_for_WHERE_IN($_POST['main_fieldlist']);
+            $same_fields_WHERE_IN = array();
+         }
+        
+
          $loop_count_main_con = 0; // ตัวแปรลูปของเงื่อนไขหลัก
          $loop_count_sub_con = 0; // ตัวแปรลูปของเงื่อนไขย่อย
+         $loop_count_sub_con2 = 0;
          $loop_count_all_con = 0; // ตัวแปรลูปของเงื่อนไขทั้งหมด main/sub
          $loop_conjection_condition_count = 0 ; // ตัวแปรลูปของตัวเชื่อมเงื่อนไขย่อย
-       
+         $loop_non_same_fields_count = 0;
+
          $sql .= ' WHERE'; 
        
          $array_main_con = array(); // ประกาศตัวแปร array
          $array_sub_con = array(); // ประกาศตัวแปร array
-           
+         $array_sub_con_same_fields = array();
+         $testvalue = '';
          foreach($_POST['condition_type_row'] as $condition_type){// ลูปเงื่อนไขทั้งหมด       
-            if($condition_type == "main_con"){// ถ้าเป็นเงื่อนไขหลัก                   
-               $loop_array_main = array(// เก็บค่าเงื่อนไข ชนิด array
-                  'oplist' =>$_POST['main_oplist'][$loop_count_main_con], 
-                  'fieldlist' =>$_POST['main_fieldlist'][$loop_count_main_con],
-                  'condition_opv' =>$_POST['main_condition_opv'][$loop_count_main_con],
-                  'valuelist' => $_POST['main_condition_value_input'][$loop_count_main_con],
-                  'value_type' => $_POST['main_condition_value_type'][$loop_count_main_con]
-               );          
-               $loop_count_main_con++;
-               $loop_count_all_con++;            
-               $sql .= generate_main_sql_condition($loop_array_main,$fields_json);// ต่อข้อความด้วยเรียกใช้ฟังก์ชั่น  
+            if($condition_type == "main_con"){// ถ้าเป็นเงื่อนไขหลัก      
+
+               if(in_array($_POST['main_fieldlist'][$loop_count_main_con],$same_fields) && $_POST['main_condition_opv'][$loop_count_main_con] == "="){
+                  
+                  $array_same_fields_to_func = array(
+                     'field_name' =>$_POST['main_fieldlist'][$loop_count_main_con],
+                     'value_type' => $_POST['main_condition_value_type'][$loop_count_main_con],
+                     'valuelist' => $_POST['main_condition_value_input'][$loop_count_main_con]
+                  );
+
+                  array_push($same_fields_WHERE_IN,$array_same_fields_to_func);
+
+                  $loop_count_main_con++;
+                  $loop_count_all_con++;           
+               }
+               else{
+                  $loop_array_main = array(// เก็บค่าเงื่อนไข ชนิด array
+                     'oplist' =>$_POST['main_oplist'][$loop_count_main_con], 
+                     'fieldlist' =>$_POST['main_fieldlist'][$loop_count_main_con],
+                     'condition_opv' =>$_POST['main_condition_opv'][$loop_count_main_con],
+                     'valuelist' => $_POST['main_condition_value_input'][$loop_count_main_con],
+                     'value_type' => $_POST['main_condition_value_type'][$loop_count_main_con]
+                  ); 
+                  $loop_non_same_fields_count++;
+                  $loop_count_main_con++;
+                  $loop_count_all_con++;           
+                  $sql .= generate_main_sql_condition($loop_array_main,$fields_json,$loop_non_same_fields_count);// ต่อข้อความด้วยเรียกใช้ฟังก์ชั่น  
+                  $testvalue.= generate_main_sql_condition($loop_array_main,$fields_json,$loop_non_same_fields_count);// ต่อข้อความด้วยเรียกใช้ฟังก์ชั่น  
+               }   
             }
-            else if($condition_type == "main_row_sub_con"){            
+            else if($condition_type == "main_row_sub_con"){// ถ้าเป็นเงื่อนไขย่อย    
+
+               if(empty($_POST['sub_con_optlist'][$loop_conjection_condition_count]) || $loop_non_same_fields_count+1 == 1){
+                  $sql.= ' (';
+               }
+               else{
+                  $sql.= $_POST['sub_con_optlist'][$loop_conjection_condition_count].' (';
+               }
+               
+               $loop_sub_non_same_fields_count = 0;
                $loop_array_sub = array();
-               $loop_array_sub2 = array();        
+               $loop_array_sub2 = array();    
+               $loop_array_fields = array();    
                $loop_count_all_con++;     
                $number_of_sub_condition = $sub_row_data_count->{"sub_con".$loop_count_all_con};     
                $loop_array_sub['conjection_condition'] = $_POST['sub_con_optlist'][$loop_conjection_condition_count];      
-               for( $i =1 ; $i <= $number_of_sub_condition ; $i++){
-                  $sub_con = array(
-                     'sub_op_list' =>$_POST['sub_oplist'][$loop_count_sub_con], 
-                     'sub_field_list' =>$_POST['sub_fieldlist'][$loop_count_sub_con],
-                     'sub_condition_opv' =>$_POST['sub_condition_opv'][$loop_count_sub_con],
-                     'sub_value_list' => $_POST['sub_condition_value_input'][$loop_count_sub_con],
-                     'sub_condition_value_type' => $_POST['sub_condition_value_type'][$loop_count_sub_con]
-                  ); 
-                  $loop_count_sub_con++;   
-                  array_push($loop_array_sub2,$sub_con);
-               }    
+               
+               for($i =1 ; $i <= $number_of_sub_condition ; $i++){
+                  array_push($loop_array_fields,$_POST['sub_fieldlist'][$loop_count_sub_con2]);
+                  $loop_count_sub_con2++;
+               }
+
+               $sub_con_same_fields = count_same_fields_for_WHERE_IN($loop_array_fields);
+               $sub_con_same_fields_WHERE_IN = array();
+
+               for($i =1 ; $i <= $number_of_sub_condition ; $i++){
+
+                  if(in_array($_POST['sub_fieldlist'][$loop_count_sub_con],$sub_con_same_fields) && $_POST['sub_condition_opv'][$loop_count_sub_con] == "="){
+                  
+                     $array_sub_con_same_fields_to_func = array(
+                        'sub_field_name' =>$_POST['main_fieldlist'][$loop_count_main_con],
+                        'sub_value_type' => $_POST['main_condition_value_type'][$loop_count_main_con],
+                        'sub_valuelist' => $_POST['main_condition_value_input'][$loop_count_main_con]
+                     );
+   
+                     array_push($sub_con_same_fields_WHERE_IN,$array_sub_con_same_fields_to_func);
+                     
+                     $loop_count_sub_con++;   
+                     
+                  }
+                  else{
+                     $sub_con = array(
+                        'sub_op_list' =>$_POST['sub_oplist'][$loop_count_sub_con], 
+                        'sub_field_list' =>$_POST['sub_fieldlist'][$loop_count_sub_con],
+                        'sub_condition_opv' =>$_POST['sub_condition_opv'][$loop_count_sub_con],
+                        'sub_value_list' => $_POST['sub_condition_value_input'][$loop_count_sub_con],
+                        'sub_condition_value_type' => $_POST['sub_condition_value_type'][$loop_count_sub_con]
+                     ); 
+                     array_push($loop_array_sub2,$sub_con);
+                    
+                     $loop_sub_non_same_fields_count++;
+                     $loop_non_same_fields_count++;
+                     $loop_count_sub_con++;   
+                     $loop_array_sub['sub_con'] = $loop_array_sub2;   
+                     $sql.= generate_sub_sql_condition2($sub_con,$fields_json,$loop_sub_non_same_fields_count);
+                     $testvalue.= generate_sub_sql_condition2($sub_con,$fields_json,$loop_sub_non_same_fields_count);
+                  }   
+               }
+              
+               // for( $i =1 ; $i <= $number_of_sub_condition ; $i++){
+               //    $sub_con = array(
+               //       'sub_op_list' =>$_POST['sub_oplist'][$loop_count_sub_con], 
+               //       'sub_field_list' =>$_POST['sub_fieldlist'][$loop_count_sub_con],
+               //       'sub_condition_opv' =>$_POST['sub_condition_opv'][$loop_count_sub_con],
+               //       'sub_value_list' => $_POST['sub_condition_value_input'][$loop_count_sub_con],
+               //       'sub_condition_value_type' => $_POST['sub_condition_value_type'][$loop_count_sub_con]
+               //    ); 
+               //    $loop_count_sub_con++;   
+               //    array_push($loop_array_sub2,$sub_con);
+               // }    
                $loop_conjection_condition_count++;  
-               $loop_array_sub['sub_con'] = $loop_array_sub2;          
-               $sql.= generate_sub_sql_condition($loop_array_sub,$fields_json);
+               $sql.= ')';
             }
          }
+
+         if(!empty($_POST['main_fieldlist'])){
+            $sql.= ' AND ';
+
+            $sql.= parse_condition_to_WHERE_IN($same_fields_WHERE_IN,$fields_json);
+            $testvalue.= parse_condition_to_WHERE_IN($same_fields_WHERE_IN,$fields_json);
+         }
+         
          $result = mysqli_query($conn,$sql);
          $query_data = array(); 
+
          if($result){
             $rowcount=mysqli_num_rows($result);
             if($rowcount > 0){
@@ -483,10 +719,12 @@ function generate_where_condition_WBS_fields($fields_json){
       }      
       $response['task_type'] = "other_task";
    }
+
    if(in_array("WBS",$fields_json)){
       $sql.= generate_last_sql($fields_json);
    }
    $response['sql'] = $sql;
    $response['HTTP_post'] = $_POST;
+   $response['test_result'] = $testvalue;
    echo json_encode($response);
 ?>
