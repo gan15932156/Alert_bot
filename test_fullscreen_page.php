9b5ca3c10ae1a2159b66dbfe2a378806b9aaaf1f
Gan <powerganoflove@hotmail.com>
2020-05-18 22:03:54 +0700

    Bug fix!!! flat layout

--- test_fullscreen_page.php
+++ test_fullscreen_page.php
@@ -1,28 +1,3 @@
 <?php
-	// $break = FALSE;
-	// while($break == FALSE)
-	// 	{
-	// 		if($now != $base)
-	// 		{
-	// 			sleep(60);
-	// 		}
-	// 		elseif($now == $base)
-	// 		{
-	// 			mail($to, $subject, $message, $headers);
-	// 			sleep(60); //In order to make the next execution == FALSE
-	// 		}
-	// 	}
-	$break = FALSE;
-	$i = 1;
-	while($break == FALSE){
-		if($i == 5){
-			sleep(1);
-			$break = true;
-		}	
-		else{
-			sleep(1);
-			echo $i."<br>";
-		}
-		$i++;
-	}
+	var_dump($_REQUEST);
 ?>
